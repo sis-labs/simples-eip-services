34581a17a38e20e01b762f4666ba8c295e8d355e
ollie314 <mehdi.lefebvre@gmail.com>
2024-07-23 09:33:04 +0200

    chore: add aliases
    Signed-off-by: ollie314 <mehdi.lefebvre@gmail.com>

---
+++ .devtools/scripts/git_aliases.sh
@@ -0,0 +1,14 @@
+git config --global alias.ci commit
+git config --global alias.cim 'commit -m'
+git config --global alias.cis 'commit -s'
+git config --global alias.cism 'commit -s -m'
+git config --global alias.br branch
+git config --global alias.brd 'branch -d'
+git config --global alias.co checkout
+git config --global alias.cob 'checkout -b'
+git config --global alias.lol 'log --oneline'
+git config --global alias.st status
+git config --global alias.stg stage
+git config --global alias.stp 'status -p'
+git config --global alias.unstage 'reset HEAD --'
+git config --global alias.us 'reset HEAD --'
