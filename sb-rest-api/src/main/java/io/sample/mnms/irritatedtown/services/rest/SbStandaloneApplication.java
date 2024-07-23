27fce7751f7cbb9bbf09791b21322cf561026178
ollie314 <mehdi.lefebvre@gmail.com>
2024-07-23 10:16:21 +0200

    chore: prepare the springboot plain rest project
    Signed-off-by: ollie314 <mehdi.lefebvre@gmail.com>

---
+++ sb-rest-api/src/main/java/io/sample/mnms/irritatedtown/services/rest/SbStandaloneApplication.java
@@ -0,0 +1,8 @@
+package io.sample.mnms.irritatedtown.services.rest;
+
+public class SbStandaloneApplication {
+
+  public static void main(final String... args) {
+    System.out.println("It works");
+  }
+}
---
+++ sb-rest-api/src/test/java/io/sample/mnms/irritatedtown/services/rest/SbStandaloneApplicationTest.java
@@ -0,0 +1,12 @@
+package io.sample.mnms.irritatedtown.services.rest;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SbStandaloneApplicationTest {
+  @Test
+  void checkOk() {
+    assertEquals(1, 1 + 3 - 2 - 1);
+  }
+}
