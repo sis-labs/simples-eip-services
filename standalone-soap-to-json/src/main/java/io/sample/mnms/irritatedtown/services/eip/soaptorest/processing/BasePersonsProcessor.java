af39868d8360ae3c41a10720b0baeda6af044c0a
ollie314 <mehdi.lefebvre@gmail.com>
2024-07-30 09:28:24 +0200

    doc: add simple documentation for baseprocessor
    Signed-off-by: ollie314 <mehdi.lefebvre@gmail.com>

--- standalone-soap-to-json/src/main/java/io/sample/mnms/irritatedtown/services/eip/soaptorest/processing/BasePersonsProcessor.java
+++ standalone-soap-to-json/src/main/java/io/sample/mnms/irritatedtown/services/eip/soaptorest/processing/BasePersonsProcessor.java
@@ -2,9 +2,24 @@ package io.sample.mnms.irritatedtown.services.eip.soaptorest.processing;
 
 import io.sample.mnms.irritatedtown.domain.services.PersonsService;
 
+/**
+ * Base class for processors which interacts with persons.
+ *
+ * @author miga
+ * @version 0.0.1
+ * @since 0.0.1
+ */
 public abstract class BasePersonsProcessor {
+  /**
+   * Service to use to interact with person data
+   */
   protected final PersonsService personsService;
 
+  /**
+   * Create a new instance of the processor.
+   *
+   * @param personsService the service to interact with person data.
+   */
   protected BasePersonsProcessor(final PersonsService personsService) {
     this.personsService = personsService;
   }
